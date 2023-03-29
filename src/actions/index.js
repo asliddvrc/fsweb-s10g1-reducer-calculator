892535868c173a4ef9ba4ca94c8175ecafd1824c
asliddvrc <a.dvrc95@gmail.com>
2023-03-29 09:36:10 +0300

    ödev teslimi

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,34 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEMORY_PLUS = "MEMORY_PLUS";
+export const MEMORY_R = "MEMORY_R";
+export const MEMORY_CLEAR = "MEMORY_CLEAR";
 
 export const addOne = () => {
-  return ({ type: ADD_ONE });
-}
+  return { type: ADD_ONE };
+};
 
 export const applyNumber = (number) => {
-  return ({ type: APPLY_NUMBER, payload: number });
-}
+  return { type: APPLY_NUMBER, payload: number };
+};
+export const changeOperation = (number) => {
+  return { type: CHANGE_OPERATION, payload: number };
+};
+
+export const clearDisplay = () => {
+  return { type: CLEAR_DISPLAY };
+};
+
+export const memoryPlus = (item) => {
+  return { type: MEMORY_PLUS, payload: item };
+};
+
+export const memoryR = (number) => {
+  return { type: MEMORY_R, payload: number };
+};
+
+export const memoryClear = () => {
+  return { type: MEMORY_CLEAR };
+};
